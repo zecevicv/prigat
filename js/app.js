07118199073301742af87b62d7aa6b034e47ec65
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-07-15 12:24:45 +0200

    [ADD] - Message Modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /* #Home Grid
   ======================================================= */
+const body = document.querySelector('body');
 const homeGrid = document.querySelector('.home-grid');
 
 // Grid Item Handler
@@ -16,6 +17,38 @@ if (homeGrid) {
   });
 }
 
+// Message Modal Handler
+if (homeGrid) {
+  const gridItems = homeGrid.querySelectorAll('.grid-item');
+
+  gridItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (!e.target.closest('.like')) {
+        const modal = document.querySelector(item.dataset.modal);
+
+        modal.classList.toggle('show');
+        body.classList.toggle('no-scroll');
+      }
+    });
+  });
+}
+
+// Modal Close
+const messageModals = document.querySelectorAll('.message-modal');
+
+if (messageModals) {
+  messageModals.forEach((modal) => {
+    modal.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (e.target.closest('.modal-close')) {
+        modal.classList.toggle('show');
+        body.classList.toggle('no-scroll');
+      }
+    });
+  });
+}
+
 // Grid Add Handler
 if (homeGrid) {
   const gridAds = homeGrid.querySelectorAll('.grid-item.ad');
@@ -24,7 +57,7 @@ if (homeGrid) {
   gridAds.forEach((ad, index) => {
     if (index !== 0) {
       rowNum = rowNum + 4;
-      ad.style.gridRow= rowNum + " / span 2";
+      ad.style.gridRow = rowNum + " / span 2";
     }
   });
 }
